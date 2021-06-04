406b6299657fe924b55ac51b8d316e82b90bfe01
chk-jxcn <chk.jxcn@gmail.com>
2021-06-05 03:05:21 +0800

    Update device_local.c

--- src/bsd/device_local.c
+++ src/bsd/device_local.c
@@ -244,6 +244,10 @@ static bool setup_device(void) {
 		device_type = DEVICE_TYPE_TUN;
 
 	case DEVICE_TYPE_TUN:
+		{
+                        const int zero = 0;
+                        ioctl(device_fd_local, TUNSIFPID, &zero, sizeof(zero));
+                }
 #ifdef TUNSIFHEAD
 		{
 			const int zero = 0;
